4a4dbeb7352c2f1fd0d011e206b675bc319e7d66
ngrah <grahnick@gmail.com>
2017-10-13 06:30:41 -0400

    first commit

---
+++ helloSwift/bridging_header.h
@@ -0,0 +1,12 @@
+//
+//  bridging_header.h
+//  
+//
+//  Created by Nick Grah on 10/13/17.
+//
+
+#ifndef bridging_header_h
+#define bridging_header_h
+
+
+#endif /* bridging_header_h */
---
+++ helloSwift/bridging_header.m
@@ -0,0 +1,9 @@
+//
+//  bridging_header.m
+//  helloSwift
+//
+//  Created by Nick Grah on 10/13/17.
+//  Copyright © 2017 Nick Grah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
